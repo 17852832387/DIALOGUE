06551ea977e037ae250c5db804ab9a821e377d1c
17852832387 <117808561+17852832387@users.noreply.github.com>
2022-11-17 10:56:18 +0800

    Update DIALOGUE.plot.R
    show colsums

--- R/DIALOGUE.plot.R
+++ R/DIALOGUE.plot.R
@@ -79,6 +79,8 @@ DIALOGUE.plot.av<-function(R,MCPs,mark.samples = NULL,d = 1,k = R$k["DIALOGUE"],
     print(m1)
     m1[is.na(m1)] <- 0
     print(m1)
+    print(colSums(m1))
+    print(colSums(m1)>0)
     m1 <- m1[colSums(m1)>0]
     print(m1)
     if(length(R$MCP.cell.types[[i]])<2){return()}
